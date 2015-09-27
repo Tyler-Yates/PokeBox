75a17837c51a9b1ac7a2141e59161b69d2d460c5
Tyler Yates <tyler.yates@utexas.edu>
2015-09-27 12:41:00 -0500

    Decode the box pokemon

--- src/main/java/poke/decode/SaveFileReader.java
+++ src/main/java/poke/decode/SaveFileReader.java
@@ -10,6 +10,8 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class SaveFileReader {
+    private static final int NUMBER_OF_PC_BOXES = 12;
+    private static final int BOX_LENGTH = 1122;
 
     public static void main(String[] args) throws IOException {
         final ClassLoader classLoader = SaveFileReader.class.getClassLoader();
@@ -34,9 +36,28 @@ public class SaveFileReader {
         final int casinoCoins = MoneyDecoder.getMoney(ByteUtil.getBytes(bytes, 0x2850, 2));
         System.out.println("Casino coins: " + casinoCoins);
 
+        System.out.println("\nParty Pokemon:");
         final List<Pokemon> party = PokemonDecoder.decodePokemonPartyList(ByteUtil.getBytes(bytes, 0x2F2C, 404));
         for (final Pokemon pokemon : party) {
             System.out.println(pokemon);
+        }
+
+        final int currentBox = (bytes[0x284C] & Byte.parseByte("01111111", 2));
+        for (int i = 0; i < NUMBER_OF_PC_BOXES; i++) {
+            System.out.println("\nBox " + (i + 1));
+
+            final int offset;
+            // The current PC Box is written into a special spot in memory
+            if (i == currentBox) {
+                offset = 0x30C0;
+            } else {
+                offset = 0x4000 + i * BOX_LENGTH;
+            }
+
+            final List<Pokemon> box = PokemonDecoder.decodeBoxPokemonList(ByteUtil.getBytes(bytes, offset, BOX_LENGTH));
+            for (final Pokemon pokemon : box) {
+                System.out.println(pokemon);
+            }
         }
     }
 }
