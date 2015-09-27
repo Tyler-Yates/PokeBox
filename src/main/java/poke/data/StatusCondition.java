3517d012f667294fa169946e92b0d4ec93f1228c
Tyler Yates <tyler.yates@utexas.edu>
2015-09-27 00:18:31 -0500

    Flesh out StatusCondition enum

--- src/main/java/poke/data/StatusCondition.java
+++ src/main/java/poke/data/StatusCondition.java
@@ -1,5 +1,27 @@
 package poke.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum StatusCondition {
-    ASLEEP, POISONED, BURNED, FROZEN, PARALYZED, NONE
+    ASLEEP, POISONED, BURNED, FROZEN, PARALYZED, NONE;
+
+    private static final Map<Byte, StatusCondition> mapping = new HashMap<>();
+
+    static {
+        mapping.put((byte) 0x00, NONE);
+        mapping.put((byte) 0x04, ASLEEP);
+        mapping.put((byte) 0x08, POISONED);
+        mapping.put((byte) 0x10, BURNED);
+        mapping.put((byte) 0x20, FROZEN);
+        mapping.put((byte) 0x40, PARALYZED);
+    }
+
+    public static StatusCondition getStatusCondition(byte b) {
+        final StatusCondition statusCondition = mapping.get(b);
+        if (statusCondition == null) {
+            return NONE;
+        }
+        return statusCondition;
+    }
 }
