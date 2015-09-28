bd8908b866c61e838fb8a2ab1b07e4b755db375b
Tyler Yates <tyler.yates@utexas.edu>
2015-09-28 15:52:28 -0500

    Fix potential NullPointerException when calculating level

--- src/main/java/poke/data/xp/XpType.java
+++ src/main/java/poke/data/xp/XpType.java
@@ -38,13 +38,13 @@ public abstract class XpType {
 
     public int getLevel(int xp) {
         final Map<Integer, Integer> xpNeededPerLevel = getXpNeededPerLevel();
-        for (int level = 2; level <= 101; level++) {
+        for (int level = 2; level <= 100; level++) {
             final int neededXp = xpNeededPerLevel.get(level);
             if (xp < neededXp) {
                 return level - 1;
             }
         }
-        return 0;
+        return 100;
     }
 
     abstract Map<Integer, Integer> getXpNeededPerLevel();
