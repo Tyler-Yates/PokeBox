05fa41b8f026f26680555b537159b18b800b85ce
Tyler Yates <tyler.yates@utexas.edu>
2015-12-16 16:43:51 -0600

    Provide better documentation in the MoneyDecoder

--- src/main/java/poke/decode/MoneyDecoder.java
+++ src/main/java/poke/decode/MoneyDecoder.java
@@ -7,7 +7,13 @@ class MoneyDecoder {
     static int getMoney(byte[] encodedMoney) {
         final StringBuilder stringBuilder = new StringBuilder();
         for (final byte b : encodedMoney) {
-            // Each byte is a binary coded decimal with two digits.
+            /*
+             Each byte is a binary coded decimal with two digits. Each byte contains two digits:
+                The first four bits correspond to the first digit
+                The last four bits correspond to the second digit
+
+             For example: 00110000 corresponds to 30
+              */
             final String string = StringUtils.leftPad(Integer.toBinaryString(b), 8, '0');
             stringBuilder.append(Integer.parseInt(string.substring(0, 4), 2));
             stringBuilder.append(Integer.parseInt(string.substring(4, 8), 2));
