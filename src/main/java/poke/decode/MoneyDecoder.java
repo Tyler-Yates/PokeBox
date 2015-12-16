440e19bea64518c219560468e77a4581c318177e
Tyler Yates <tyler.yates@utexas.edu>
2015-12-16 16:26:33 -0600

    Fix money decoding

--- src/main/java/poke/decode/MoneyDecoder.java
+++ src/main/java/poke/decode/MoneyDecoder.java
@@ -1,12 +1,16 @@
 package poke.decode;
 
+import org.apache.commons.lang.StringUtils;
+
 class MoneyDecoder {
 
     static int getMoney(byte[] encodedMoney) {
         final StringBuilder stringBuilder = new StringBuilder();
         for (final byte b : encodedMoney) {
-            final String string = Integer.toHexString(Byte.toUnsignedInt(b));
-            stringBuilder.append(string);
+            // Each byte is a binary coded decimal with two digits.
+            final String string = StringUtils.leftPad(Integer.toBinaryString(b), 8, '0');
+            stringBuilder.append(Integer.parseInt(string.substring(0, 4), 2));
+            stringBuilder.append(Integer.parseInt(string.substring(4, 8), 2));
         }
         return Integer.parseInt(stringBuilder.toString());
     }
